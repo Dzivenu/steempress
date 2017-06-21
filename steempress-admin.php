1103c2b1ce7356782feb675a6dcc772b392e4c4a
amexperts <am.experts.online@gmail.com>
2017-06-22 06:17:35 +1200

    Added skeleton files.

---
+++ steempress-admin.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! defined('ABSPATH') ) exit;
+
+add_action('admin_menu', 'steempress_menu');
+
+function steempress_menu() {
+
+}
---
+++ steempress.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: SteemPress
+* Plugin URI: http://steempress.amexperts.com
+* Description: Plugin for wordpress that integrates with steem
+* Version: 10.1
+* Author: Adam Martin
+* Author URI: http://www.amexperts.com
+* License: GPLv3 or later
+*/
+
+/** Copyright 2017 AM Experts
