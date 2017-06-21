7d02dc91ad03d4288f99a71c786183131fd30a95
amexperts <am.experts.online@gmail.com>
2017-06-22 07:48:32 +1200

    Added Admin Menu

--- steempress.php
+++ steempress.php
@@ -4,10 +4,28 @@
 * Plugin Name: SteemPress
 * Plugin URI: http://steempress.amexperts.com
 * Description: Plugin for wordpress that integrates with steem
-* Version: 10.1
+* Version: 0.1
 * Author: Adam Martin
 * Author URI: http://www.amexperts.com
 * License: GPLv3 or later
 */
 
-/** Copyright 2017 AM Experts
+/*
+  Copyright 2017 AM Experts
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+if ( ! defined('ABSPATH') ) exit;
+
+include dirname( __FILE__ ) .'/steempress-admin.php';
